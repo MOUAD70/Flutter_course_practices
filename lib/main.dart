c180a1fe40a7e2b79eb6ab8130468423c3e723bf
mouad <mouadinfo30@gmail.com>
2026-03-08 23:34:08 +0000

    #PT1: Basics --> ValueNotifier

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course/views/data/notifiers.dart';
+import 'package:flutter_course/views/widget_tree.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.purple,
+              brightness: isDarkMode ? Brightness.light : Brightness.dark,
+            ),
+          ),
+          home: WidgetTree(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/views/data/notifiers.dart
@@ -0,0 +1,10 @@
+// ValueNotifier: Holds the Data
+// ValueListenableBuilder: Listen to the Data (No need to use setState)
+
+import 'package:flutter/material.dart';
+
+ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
+
+ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(false);
+
+
